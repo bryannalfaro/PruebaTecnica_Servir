0b583c93a982ad93487c4b7ecf68fd63ee57a573
Bryann <alf19372@uvg.edu.gt>
2024-05-28 22:33:14 -0600

    ADD: creating the html files for the development

--- app.py
+++ app.py
@@ -11,5 +11,33 @@ def hello():
 def select_entity():
     return render_template('select-entity.html')
 
+#employees
+@app.route('/all-employees')
+def all_employees():
+    return render_template('all-employees.html')
+@app.route('/new-employee')
+def new_employee():
+    return render_template('new-employee.html')
+@app.route('/del-employee')
+def del_employee():
+    return render_template('del-employee.html')
+@app.route('/edit-employee')
+def edit_employee():
+    return render_template('edit-employee.html')
+
+#departments
+@app.route('/all-departments')
+def all_departments():
+    return render_template('all-departments.html')
+@app.route('/new-department')
+def new_department():
+    return render_template('new-department.html')
+@app.route('/del-department')
+def del_department():
+    return render_template('del-department.html')
+@app.route('/edit-department')
+def edit_department():
+    return render_template('edit-department.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
